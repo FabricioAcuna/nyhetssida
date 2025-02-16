3bb070de0764745884713bba4b58b113b2192689
Jenny Digné <jenny_digne@hotmail.com>
2025-02-16 18:32:06 +0100

    la in länk till huvudsidan

--- components/Header.js
+++ components/Header.js
@@ -27,6 +27,11 @@ export default function Header() {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-white"
             >
               <li>
+                <Link href={"/"}>
+                  <h2 className="font-semibold">News</h2>
+                </Link>
+              </li>
+              <li>
                 <Link href={"/sports"}>
                   <h2 className="font-semibold">Sports</h2>
                 </Link>
@@ -60,6 +65,11 @@ export default function Header() {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href={"/"}>
+                <h2 className="font-semibold text-xl">News</h2>
+              </Link>
+            </li>
             <li>
               <Link href={"/sports"}>
                 <h2 className="font-semibold text-xl">Sports</h2>
