1ad76ddb363051845586a5102de5e5e0f7ec17b5
FabricioAcuna <fabricio.acuna@chasacademy.se>
2025-02-17 23:11:48 +0100

    text

--- pages/saved.js
+++ pages/saved.js
@@ -15,9 +15,16 @@ export default function SavedArticlesPage() {
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-10">
           {savedArticles.map((article) => (
-            <div key={article.article_id} className="card bg-white shadow-xl p-4 border-y-2 border-red-500">
-              <Link href={`/newsArticles/${article.category}/${article.article_id}`}>
-                <h2 className="text-2xl font-bold hover:underline">{article.title}</h2>
+            <div
+              key={article.article_id}
+              className="card bg-white shadow-xl p-4 border-y-2 border-red-500"
+            >
+              <Link
+                href={`/newsArticles/${article.category}/${article.article_id}`}
+              >
+                <h2 className="text-2xl font-bold hover:underline text-black">
+                  {article.title}
+                </h2>
               </Link>
               <img
                 src={article.image_url || defaultImg}
